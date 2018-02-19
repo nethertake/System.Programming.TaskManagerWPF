eda4e67ca33aac6870091db3e1988a52b38493eb
СамигулинТ <@403_8_>
2018-02-19 17:55:49 +0600

    444

--- TaskManagerWPF/MainWindow.xaml.cs
+++ TaskManagerWPF/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
 
 namespace TaskManagerWPF
 {
@@ -23,6 +28,51 @@ namespace TaskManagerWPF
         public MainWindow()
         {
             InitializeComponent();
+            GetData();
+
         }
+        public List<ProcessClass> processClasses = new List<ProcessClass>();
+        public void GetData()
+        {
+         
+            List<Process> listProcesses = Process.GetProcesses(".").ToList();
+            foreach (var item in listProcesses)
+            {
+                ProcessClass pc = new ProcessClass();
+                pc.id = item.Id;
+                pc.name = item.ProcessName;
+                pc.threadcount = item.Threads.Count;
+                pc.memory = item.WorkingSet64;
+                processClasses.Add(pc);
+               
+            }
+
+            ListViewTaskManager.ItemsSource = processClasses;
+            
+
+        }
+
+        public class ProcessClass
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public int threadcount { get; set; }
+            public long memory { get; set; }
+
+            public override string ToString()
+            {
+                return $"{memory} - {name}";
+            }
+        }
+
+  
+
+
+
     }
+
+       
+    
+
 }
+
